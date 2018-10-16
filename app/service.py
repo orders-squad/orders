954c1aee87b7249275e812fabfe938f6f7d4fc14
Divisha Jaiswal <divishajaiswal@Divishas-MBP.fios-router.home>
2018-10-15 23:52:29 -0400

    added method to delete an order

--- app/service.py
+++ app/service.py
@@ -105,6 +105,25 @@ def update_orders(order_id):
     order.save()
     return make_response(jsonify(order.serialize()), status.HTTP_200_OK)
 
+######################################################################
+# DELETE AN EXISTING ORDER
+######################################################################
+@app.route('/orders/<int:order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    """
+    Delete an Order
+    This endpoint will delete an Order based the body that is posted
+    """
+    check_content_type('application/json')
+    order = Order.find(order_id)
+    if not order:
+        raise NotFound("Order with id '{}' was not found.".format(order_id))
+    else:
+        order.delete()
+    return make_response('', status.HTTP_204_NO_CONTENT)
+
+    
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
