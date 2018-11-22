54abf3dc19a8fbcdc3720a7fa940ac0cbb453e5c
Michael Gold <goldmichael@gmail.com>
2018-11-22 15:16:57 -0500

    read environment variables for the database and initialize sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -6,11 +6,31 @@ This module also sets up the logging to be used with gunicorn
 """
 import logging
 from flask import Flask
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+DB_NAME = get_env_variable('DB_NAME')
+DB_USER = get_env_variable('DB_USER')
+DB_PASS = get_env_variable('DB_PASS')
+DB_ADDR = get_env_variable('DB_ADDR')
 
 # Create Flask application
 app = Flask(__name__)
 # We'll just use SQLite here so we don't need an external database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../db/development.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=DB_USER,pw=DB_PASS,url=DB_ADDR,db=DB_NAME)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'please, tell nobody... Shhhh'
 app.config['LOGGING_LEVEL'] = logging.INFO
