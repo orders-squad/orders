b2600ed92bf380b8c517a2bc75d93fe715523d72
Michael Gold <goldmichael@gmail.com>
2018-11-22 15:49:37 -0500

    use postgres for tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -18,7 +18,26 @@ from mock import MagicMock, patch
 from app.models import Order, OrderItem, DataValidationError, db
 import app.service as service
 
-DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///../db/test.db')
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '../.env'))
+
+def get_env_variable(name):
+    try:
+        return os.environ[name]
+    except KeyError:
+        message = "Expected environment variable '{}' not set.".format(name)
+        raise Exception(message)
+
+
+DB_NAME = get_env_variable('DB_NAME')
+DB_USER = get_env_variable('DB_USER')
+DB_PASS = get_env_variable('DB_PASS')
+DB_ADDR = get_env_variable('DB_ADDR')
+
+
+DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=DB_USER,pw=DB_PASS,url=DB_ADDR,db=DB_NAME)
 
 ######################################################################
 #  T E S T   C A S E S
