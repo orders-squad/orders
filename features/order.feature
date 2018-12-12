93f97a9cb16d980a9e2534770f55d3436d028977
Divisha Jaiswal <dj1242@nyu.edu>
2018-12-12 03:30:30 -0500

    adding Cancel Scenario for BDD and clean the code

--- features/order.feature
+++ features/order.feature
@@ -6,8 +6,9 @@ Feature: The order store service back-end
 Background:
 	Given the following orders
 	    | cust_id | prod_id | prod_name | prod_qty | prod_price | status  | 
-	    | 1       | 1 	    | echo dot  | 2        | 50.5       | ordered |   
-	    | 2       | 2       | kindle    | 3        | 40.5       | ordered |   
+	    | 1       | 1 	| echo dot  | 2        | 50.5       | ordered |   
+	    | 2       | 2       | kindle    | 3        | 40.5       | ordered |
+	    | 3       | 3       | backpack  | 1        | 23.4       | ordered |   
 
 Scenario: The server is running
 	When I visit the "Home Page"
@@ -28,7 +29,7 @@ Scenario: Create an Order
 	And I set the "Price" to "200"
 	And I set the "status" to "ordered"
 	When I press the "create" item button
-	And I set the "customer id" to "123"
+	And I set the "customer id" to "12"
 	And I press the "Create" order button
 	Then I should see the message "Success"
 
@@ -41,45 +42,65 @@ Scenario: List all Orders
 
 Scenario: Update an Order
 	When I visit the "Home Page"
-	And I set the "order_Id" to "1"
-	And I set the "customer id" to "1"
+	And I set the "order_Id" to "3"
+	And I set the "customer id" to "3"
 	And I press the "search" order button
-	When I change "order_Id" to "3"
-	And I set the "customer id" to "1"
-	And I press the "update" order button
+	And I press the "retrieve" order button
+	And I set the "order_Id" to "3"
+    	When I change "customer id" to "2"
+    	And I press the "update" order button
 	Then I should see the message "Success"
 
 Scenario: Read an Order
-	When I visit the "Home Page"
-    	And I set the "order_id" to "1"
-	And I set the "customer id" to "1"
-    	And I press the "search" order button
-	And I press the "retrieve" order button
-    	Then I should see "1" in the "customer id" field	
-	Then I should see "1" in the "order_id" field
-    	Then I should see "ordered" in the "order_status" field
-	Then I should see the message "Success"
+    	When I visit the "Home Page"
+        And I set the "order_id" to "1"
+    	And I set the "customer id" to "1"
+        And I press the "search" order button
+    	And I press the "retrieve" order button
+        Then I should see "1" in the "customer id" field    
+    	Then I should see "1" in the "order_id" field
+        Then I should see "ordered" in the "order_status" field
+    	Then I should see the message "Success"
+
+Scenario: Delete an Order
+        When I visit the "Home Page"
+        And I set the "order_id" to "2"
+    And I set the "customer id" to "2"
+        And I press the "search" order button
+    And I press the "retrieve" order button 
+        And I press the "delete" order button
+        Then I should see the message "Success"
+
+Scenario: Cancel a order
+        When I visit the "Home Page"
+        And I set the "order_id" to "2"
+    And I set the "customer id" to "2"
+        And I press the "search" order Button
+        And I press the "retrieve" order button
+        And I press the "cancel" order button
+        Then I should see "canceled" in the "order_status" field
+        And I should see the message "Order has been Canceled!"
 
 Scenario: List all items
-    	When I visit the "Home Page"
-    	And I press the "list" item button
-    	Then I should see "kindle" in the item results
-    	And I should see "2" in the item results
-    	And I should not see "pepper" in the item results
+        When I visit the "Home Page"
+        And I press the "list" item button
+        Then I should see "kindle" in the item results
+        And I should see "2" in the item results
+        And I should not see "pepper" in the item results
 
 Scenario: Query an item
-    	When I visit the "Home Page"
-    	And I set the "name" to "kindle"
-    	And I press the "search" item button
-    	Then I should see the message "Success"
-    	And I should see "40.5" in the item results
+        When I visit the "Home Page"
+        And I set the "name" to "kindle"
+        And I press the "search" item button
+        Then I should see the message "Success"
+        And I should see "40.5" in the item results
 
 Scenario: Read an item
-   		When I visit the "Home Page"
-   		And I set the "name" to "kindle"
-   		And I press the "search" item button
-   		Then I should see "2" in the "product_id" field
-   		Then I should see "kindle" in the "name" field
-   		Then I should see "3" in the "quantity" field
-   		Then I should see "40.5" in the "price" field
-   		Then I should see the message "Success"
+        When I visit the "Home Page"
+        And I set the "name" to "kindle"
+        And I press the "search" item button
+        Then I should see "2" in the "product_id" field
+        Then I should see "kindle" in the "name" field
+        Then I should see "3" in the "quantity" field
+        Then I should see "40.5" in the "price" field
+        Then I should see the message "Success"
--- features/steps/order_steps.py
+++ features/steps/order_steps.py
@@ -18,54 +18,83 @@ BASE_URL = getenv('BASE_URL', 'http://localhost:5000')
 
 @given(u'the following orders')
 def step_impl(context):
-	""" Delete all Orders and load new ones """
-	headers = {'Content-Type': 'application/json'}
-	#context.resp = requests.delete(context.base_url + '/orders/reset')
-	#expect(context.resp.status_code).to_equal(204)
-	create_url = context.base_url + '/orders'
-	for row in context.table:
-		data = {
-			"cust_id": row['cust_id'],
-			"items": [{
-				"prod_id": row['prod_id'],
-				"status": row['status'],
-				"prod_name":  row['prod_name'],
-				"prod_qty":  row['prod_qty'],
-				"prod_price": row['prod_price']
-				}]
-			}
-		payload = json.dumps(data)
-		context.resp = requests.post(create_url, data=payload, headers=headers)
-		expect(context.resp.status_code).to_equal(201)
+    """ Delete all Orders and load new ones """
+    headers = {'Content-Type': 'application/json'}
+    #context.resp = requests.delete(context.base_url + '/orders/reset')
+    #expect(context.resp.status_code).to_equal(204)
+    create_url = context.base_url + '/orders'
+    for row in context.table:
+        data = {
+            "cust_id": row['cust_id'],
+            "items": [{
+                "prod_id": row['prod_id'],
+                "status": row['status'],
+                "prod_name":  row['prod_name'],
+                "prod_qty":  row['prod_qty'],
+                "prod_price": row['prod_price']
+                }]
+            }
+        payload = json.dumps(data)
+        context.resp = requests.post(create_url, data=payload, headers=headers)
+        expect(context.resp.status_code).to_equal(201)
 
 @when(u'I visit the "Home Page"')
 def step_impl(context):
-	""" Make a call to the base URL """
-	context.driver.get(context.base_url)
-	# context.driver.save_screenshot('home_page.png')
+    """ Make a call to the base URL """
+    context.driver.get(context.base_url)
 
 @then(u'I should see "{message}" in the title')
 def step_impl(context, message):
-	""" Check the document title for a message """
-	expect(context.driver.title).to_contain(message)
+    """ Check the document title for a message """
+    expect(context.driver.title).to_contain(message)
 
 @then(u'I should not see "{message}"')
 def step_impl(context, message):
-	error_msg = "I should not see '%s' in '%s'" % (message, context.resp.text)
-	ensure(message in context.resp.text, False, error_msg)
+    error_msg = "I should not see '%s' in '%s'" % (message, context.resp.text)
+    ensure(message in context.resp.text, False, error_msg)
 
 @when(u'I set the "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
-	element_id = element_name.lower()
-	if element_id == 'customer id':
-		element_id = 'cust_id'
-	elif element_id == 'product id':
-		element_id = 'prod_id'
-	else:
-		element_id = 'item_' + element_id
-	element = context.driver.find_element_by_id(element_id)
-	# element.clear()
-	element.send_keys(text_string)
+    element_id = element_name.lower()
+    if element_id == 'customer id':
+        element_id = 'cust_id'
+    elif element_id == 'product id':
+        element_id = 'prod_id'
+    else:
+        element_id = 'item_' + element_id
+    element = context.driver.find_element_by_id(element_id)
+    element.send_keys(text_string)
+
+@then(u'I should see "{text_string}" in the "{element_name}" field')
+def step_impl(context, text_string, element_name):
+    element_id = element_name.lower()
+    if element_id == 'customer id':
+        element_id = 'cust_id'
+    elif element_id == 'product id':
+        element_id = 'prod_id'
+    else:
+        element_id = 'item_' + element_id
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
+    )
+    expect(found).to_be(True)
+
+@when(u'I change "{element_name}" to "{text_string}"')
+def step_impl(context, element_name, text_string):
+    element_id = element_name.lower()
+    if element_id == 'customer id':
+        element_id = 'cust_id'
+    elif element_id == 'product id':
+        element_id = 'prod_id'
+    else:
+        element_id = 'item_' + element_id
+    element = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    element.send_keys(text_string)
 
 
 ##################################################################
@@ -77,121 +106,78 @@ def step_impl(context, element_name, text_string):
 ##################################################################
 @when(u'I press the "{button}" order button')
 def step_impl(context, button):
-	button_id = button.lower() + '-btn'
+    button_id = button.lower() + '-btn'
 
-	if button == "retrieve":
-		time.sleep(6)
-		element = context.driver.find_element_by_id('order_id').text
+    if button == "retrieve":
+        time.sleep(6)
+        element = context.driver.find_element_by_id('order_id').text
 
-		found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-			expected_conditions.text_to_be_present_in_element(
-				(By.ID, 'order_id'), element
-			)
-		)
-		expect(found).to_be(True)
-		context.driver.find_element_by_id(button_id).click()
-	else:
-		context.driver.find_element_by_id(button_id).click()
+        found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'order_id'), element
+            )
+        )
+        expect(found).to_be(True)
+        context.driver.find_element_by_id(button_id).click()
+    else:
+        context.driver.find_element_by_id(button_id).click()
 
 @then(u'I should see the message "{message}"')
 def step_impl(context, message):
-	#element = context.driver.find_element_by_id('flash_message')
-	#expect(element.text).to_contain(message)
-	found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-		expected_conditions.text_to_be_present_in_element(
-			(By.ID, 'flash_message'),
-			message
-		)
-	)
-	expect(found).to_be(True)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
 
 @then(u'I should see "{name}" in the results')
 def step_impl(context, name):
-	#element = context.driver.find_element_by_id('search_results')
-	#expect(element.text).to_contain(name)
-	found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-		expected_conditions.text_to_be_present_in_element(
-			(By.ID, 'order_results'),
-			name
-		)
-	)
-	expect(found).to_be(True)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'order_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
 
 @then(u'I should not see "{name}" in the results')
 def step_impl(context, name):
-	element = context.driver.find_element_by_id('order_results')
-	error_msg = "I should not see '%s' in '%s'" % (name, element.text)
-	ensure(name in element.text, False, error_msg)
+    element = context.driver.find_element_by_id('order_results')
+    error_msg = "I should not see '%s' in '%s'" % (name, element.text)
+    ensure(name in element.text, False, error_msg)
 
 @when(u'I press the "{button}" item button')
 def step_impl(context, button):
-	button_id = button.lower() + '-btn-item'
+    button_id = button.lower() + '-btn-item'
 
-	if button == "retrieve":
-		time.sleep(5)
-		element = context.driver.find_element_by_id('prod_id').text
+    if button == "retrieve":
+        time.sleep(5)
+        element = context.driver.find_element_by_id('prod_id').text
 
-		found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-			expected_conditions.text_to_be_present_in_element(
-				(By.ID, 'prod_id'), element
-			)
-		)
-		expect(found).to_be(True)
-		context.driver.find_element_by_id(button_id).click()
-	else:
-		context.driver.find_element_by_id(button_id).click()
-
-@then(u'I should see "{text_string}" in the "{element_name}" field')
-def step_impl(context, text_string, element_name):
-	element_id = element_name.lower()
-	if element_id == 'customer id':
-		element_id = 'cust_id'
-	elif element_id == 'product id':
-		element_id = 'prod_id'
-	else:
-		element_id = 'item_' + element_id
-	#element = context.driver.find_element_by_id(element_id)
-	found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-		expected_conditions.text_to_be_present_in_element_value(
-			(By.ID, element_id),
-			text_string
-		)
-	)
-	#expect(element.get_attribute('value')).to_equal(text_string)
-	expect(found).to_be(True)
-
-@when(u'I change "{element_name}" to "{text_string}"')
-def step_impl(context, element_name, text_string):
-	element_id = element_name.lower()
-	#element = context.driver.find_element_by_id(element_id)
-	element = WebDriverWait(context.driver, WAIT_SECONDS).until(
-		expected_conditions.presence_of_element_located((By.ID, element_id))
-	)
-	# element.clear()
-	element.send_keys(text_string)
+        found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.ID, 'prod_id'), element
+            )
+        )
+        expect(found).to_be(True)
+        context.driver.find_element_by_id(button_id).click()
+    else:
+        context.driver.find_element_by_id(button_id).click()
 
 @then(u'I should see "{name}" in the item results')
 def step_impl(context, name):
-	found = WebDriverWait(context.driver, 40).until(
-		expected_conditions.text_to_be_present_in_element(
-			(By.ID, 'item_results'),
-			name
-		)
-	)
-	expect(found).to_be(True)
+    found = WebDriverWait(context.driver, 40).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'item_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
 
 @then(u'I should not see "{name}" in the item results')
 def step_impl(context, name):
-	element = context.driver.find_element_by_id('item_results')
-	error_msg = "I should not see '%s' in '%s'" % (name, element.text)
-	ensure(name in element.text, False, error_msg)
-
-
-
-
-
-
-
-	
-
-
+    element = context.driver.find_element_by_id('item_results')
+    error_msg = "I should not see '%s' in '%s'" % (name, element.text)
+    ensure(name in element.text, False, error_msg)
